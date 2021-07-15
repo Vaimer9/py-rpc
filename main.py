cdee5ca3f8699f0cb8df905e244f42cc2f71aa9e
Vaimer9 <vaim3r9@gmail.com>
2021-07-15 08:29:57 +0530

    Made the code compatible with the json code

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 from pypresence import Presence
 import time
+import json
+
+with open("config.json", "r") as file:
+    data = json.loads(file.read())
+
+# This chonky piece of code just gets all the values from the json so dont freak out in here
+ID =  data["id"]
+state = data["attributes"]["state"]
+details = data["attributes"]["details"]
+large_image = data["attributes"]["large_image"]
+small_image = data["attributes"]["small_image"]
+large_image_tooltip = data["attributes"]["large_image_tooltip"]
+small_image_tooltip =  data["attributes"]["small_image_tooltip"]
+label =  data["button_attributes"]["button1"][0]["label"]
+url =  data["button_attributes"]["button1"][1]["url"]
+
+
 
 # Create a Presence instance
-vaimer = Presence(client_id="834822755752345611") 
+client = Presence(client_id=ID) 
 
 # This method logs into your ID and starts the loop
-vaimer.connect()
+client.connect()
 
-# This will be the place where all of the shit goes down
-vaimer.update(state="GET FREE BOBUX",
-              details="GET FREE ROBUX",
-              large_image="aaamogus",
-              buttons=[{"label": "CLAIM NOW",
-                        "url":"https://youtu.be/dQw4w9WgXcQ" }] )
-
+# This will be the place where all of the stuff goes down
+client.update(state=state,
+              details=details,
+              large_image=large_image,
+              buttons=[{"label": f"{label}","url":rf"{url}" }]
+)
+print("The presence is now online")
 # This is the mainloop
 while True:
     time.sleep(15)
-
